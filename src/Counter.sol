2638e7e759b143d9e6cba1e2588e1c7a267f6d0f
Austin Adams <36116882+aadams@users.noreply.github.com>
2025-03-31 18:32:16 +0200

    fix: add to install new stuff

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,188 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
+
+import { IUniswapV3Factory } from "@v3-core/interfaces/IUniswapV3Factory.sol";
+import { IUniswapV3Pool } from "@v3-core/interfaces/IUniswapV3Pool.sol";
+import { IUniswapV3MintCallback } from "@v3-core/interfaces/callback/IUniswapV3MintCallback.sol";
+import { TickMath } from "@v4-core/libraries/TickMath.sol";
+import { LiquidityAmounts } from "@v4-core-test/utils/LiquidityAmounts.sol";
+import { SqrtPriceMath } from "@v4-core/libraries/SqrtPriceMath.sol";
+import { FullMath } from "@v4-core/libraries/FullMath.sol";
+import { ERC20, SafeTransferLib } from "@solmate/utils/SafeTransferLib.sol";
+import { IPoolInitializer } from "src/interfaces/IPoolInitializer.sol";
+import { ImmutableAirlock } from "src/base/ImmutableAirlock.sol";
 
 contract Counter {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+
+    function uniswapV3MintCallback(uint256 amount0Owed, uint256 amount1Owed, bytes calldata data) external {
+        CallbackData memory callbackData = abi.decode(data, (CallbackData));
+        address pool = factory.getPool(callbackData.asset, callbackData.numeraire, callbackData.fee);
+
+        require(msg.sender == pool, OnlyPool());
+
+        ERC20(callbackData.asset).safeTransferFrom(address(airlock), pool, amount0Owed == 0 ? amount1Owed : amount0Owed);
     }
 
-    function increment() public {
-        number++;
+    function alignTickToTickSpacing(bool isToken0, int24 tick, int24 tickSpacing) internal pure returns (int24) {
+        if (isToken0) {
+            // Round down if isToken0
+            if (tick < 0) {
+                // If the tick is negative, we round up (negatively) the negative result to round down
+                return (tick - tickSpacing + 1) / tickSpacing * tickSpacing;
+            } else {
+                // Else if positive, we simply round down
+                return tick / tickSpacing * tickSpacing;
+            }
+        } else {
+            // Round up if isToken1
+            if (tick < 0) {
+                // If the tick is negative, we round down the negative result to round up
+                return tick / tickSpacing * tickSpacing;
+            } else {
+                // Else if positive, we simply round up
+                return (tick + tickSpacing - 1) / tickSpacing * tickSpacing;
+            }
+        }
+    }
+
+    /// @notice Calculates the final LP position that extends from the far tick to the pool's min/max tick
+    /// @dev This position ensures price equivalence between Uniswap v2 and v3 pools beyond the LBP range
+    function calculateLpTail(
+        uint16 id,
+        int24 tickLower,
+        int24 tickUpper,
+        bool isToken0,
+        uint256 reserves,
+        uint256 bondingAssetsRemaining,
+        int24 tickSpacing
+    ) internal pure returns (LpPosition memory lpTail) {
+        int24 tailTick = isToken0 ? tickUpper : tickLower;
+
+        uint160 sqrtPriceAtTail = TickMath.getSqrtPriceAtTick(tailTick);
+
+        uint128 lpTailLiquidity = LiquidityAmounts.getLiquidityForAmounts(
+            sqrtPriceAtTail,
+            TickMath.MIN_SQRT_PRICE,
+            TickMath.MAX_SQRT_PRICE,
+            isToken0 ? bondingAssetsRemaining : reserves,
+            isToken0 ? reserves : bondingAssetsRemaining
+        );
+
+        int24 posTickLower = isToken0 ? tailTick : alignTickToTickSpacing(isToken0, TickMath.MIN_TICK, tickSpacing);
+        int24 posTickUpper = isToken0 ? alignTickToTickSpacing(isToken0, TickMath.MAX_TICK, tickSpacing) : tailTick;
+
+        require(posTickLower < posTickUpper, InvalidTickRangeMisordered(posTickLower, posTickUpper));
+
+        lpTail = LpPosition({ tickLower: posTickLower, tickUpper: posTickUpper, liquidity: lpTailLiquidity, id: id });
+    }
+
+    /// @notice Calculates the distribution of liquidity positions across tick ranges
+    /// @dev For example, with 1000 tokens and 10 bins starting at tick 0:
+    ///      - Creates positions: [0,10], [1,10], [2,10], ..., [9,10]
+    ///      - Each position gets an equal share of tokens (100 tokens each)
+    ///      This creates a linear distribution of liquidity across the tick range
+    function calculateLogNormalDistribution(
+        int24 tickLower,
+        int24 tickUpper,
+        int24 tickSpacing,
+        bool isToken0,
+        uint16 totalPositions,
+        uint256 totalAmtToBeSold
+    ) internal pure returns (LpPosition[] memory, uint256) {
+        int24 farTick = isToken0 ? tickUpper : tickLower;
+        int24 closeTick = isToken0 ? tickLower : tickUpper;
+
+        int24 spread = tickUpper - tickLower;
+
+        uint160 farSqrtPriceX96 = TickMath.getSqrtPriceAtTick(farTick);
+        uint256 amountPerPosition = FullMath.mulDiv(totalAmtToBeSold, WAD, totalPositions * WAD);
+        uint256 totalAssetsSold;
+        LpPosition[] memory newPositions = new LpPosition[](totalPositions + 1);
+        uint256 reserves;
+
+        for (uint256 i; i < totalPositions; i++) {
+            // calculate the ticks position * 1/n to optimize the division
+            int24 startingTick = isToken0
+                ? closeTick + int24(uint24(FullMath.mulDiv(i, uint256(uint24(spread)), totalPositions)))
+                : closeTick - int24(uint24(FullMath.mulDiv(i, uint256(uint24(spread)), totalPositions)));
+
+            // round the tick to the nearest bin
+            startingTick = alignTickToTickSpacing(isToken0, startingTick, tickSpacing);
+
+            if (startingTick != farTick) {
+                uint160 startingSqrtPriceX96 = TickMath.getSqrtPriceAtTick(startingTick);
+
+                // if totalAmtToBeSold is 0, we skip the liquidity calculation as we are burning max liquidity
+                // in each position
+                uint128 liquidity;
+                if (totalAmtToBeSold != 0) {
+                    liquidity = isToken0
+                        ? LiquidityAmounts.getLiquidityForAmount0(startingSqrtPriceX96, farSqrtPriceX96, amountPerPosition)
+                        : LiquidityAmounts.getLiquidityForAmount1(farSqrtPriceX96, startingSqrtPriceX96, amountPerPosition);
+
+                    totalAssetsSold += (
+                        isToken0
+                            ? SqrtPriceMath.getAmount0Delta(startingSqrtPriceX96, farSqrtPriceX96, liquidity, true)
+                            : SqrtPriceMath.getAmount1Delta(farSqrtPriceX96, startingSqrtPriceX96, liquidity, true)
+                    );
+
+                    // note: we keep track how the theoretical reserves amount at that time to then calculate the breakeven liquidity amount
+                    // once we get to the end of the loop, we will know exactly how many of the reserve assets have been raised, and we can
+                    // calculate the total amount of reserves after the endTick which makes swappers and LPs indifferent between Uniswap v2 (CPMM) and Uniswap v3 (CLAMM)
+                    // we can then bond the tokens to the Uniswap v2 pool by moving them over to the Uniswap v3 pool whenever possible, but there is no rush as it goes up
+                    reserves += (
+                        isToken0
+                            ? SqrtPriceMath.getAmount1Delta(
+                                farSqrtPriceX96,
+                                startingSqrtPriceX96,
+                                liquidity,
+                                false // round against the reserves to undercount eventual liquidity
+                            )
+                            : SqrtPriceMath.getAmount0Delta(
+                                startingSqrtPriceX96,
+                                farSqrtPriceX96,
+                                liquidity,
+                                false // round against the reserves to undercount eventual liquidity
+                            )
+                    );
+                }
+
+                newPositions[i] = LpPosition({
+                    tickLower: farSqrtPriceX96 < startingSqrtPriceX96 ? farTick : startingTick,
+                    tickUpper: farSqrtPriceX96 < startingSqrtPriceX96 ? startingTick : farTick,
+                    liquidity: liquidity,
+                    id: uint16(i)
+                });
+            }
+        }
+
+        require(totalAssetsSold <= totalAmtToBeSold, CannotMintZeroLiquidity());
+
+        return (newPositions, reserves);
+    }
+
+    function mintPositions(
+        address asset,
+        address numeraire,
+        uint24 fee,
+        address pool,
+        LpPosition[] memory newPositions,
+        uint16 numPositions
+    ) internal {
+        for (uint256 i; i <= numPositions; i++) {
+            IUniswapV3Pool(pool).mint(
+                address(this),
+                newPositions[i].tickLower,
+                newPositions[i].tickUpper,
+                newPositions[i].liquidity,
+                abi.encode(CallbackData({ asset: asset, numeraire: numeraire, fee: fee }))
+            );
+        }
+    }
+
+    function checkPoolParams(int24 tick, int24 tickSpacing) internal pure {
+        if (tick % tickSpacing != 0) revert InvalidTickRange(tick, tickSpacing);
     }
 }
